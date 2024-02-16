e36e5155a85b8baa40a1d22390cdc206033ff17e
Gabe Fierro <gtfierro@mines.edu>
2024-02-16 13:26:46 -0700

    fix deps

--- build_examples.sh
+++ build_examples.sh
@@ -33,4 +33,3 @@ python tools/generate-queries.py
 # for each filename in the models/ directory, run tools/compile.py -o models/compiled/<filename>.ttl
 mkdir -p models/compiled
 make compile-models
-echo "=> Compiled models"
