d2e2c80ecf767ad54f3cd53568936ac2e4d83696
Gabe Fierro <gtfierro@mines.edu>
2025-07-31 16:24:17 -0600

    reducing ontoenv setup

--- tools/compile.py
+++ tools/compile.py
@@ -8,7 +8,7 @@ from brick_tq_shacl import infer, validate
 import rdflib
 
 graph = rdflib.Graph()
-cfg = ontoenv.Config(["models/nrel-example.ttl", "ontologies"], offline=False, strict=False, includes=['*.ttl'])
+cfg = ontoenv.Config(["models", "ontologies"], offline=False, strict=False, includes=['*.ttl'], excludes=["models/compiled/*.ttl", "models/withimports/*.ttl"])
 env = ontoenv.OntoEnv(cfg, read_only=True, recreate=False)
 
 if __name__ == "__main__":
