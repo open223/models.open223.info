801bbfd11dbd27b0418ae67b51f53be63256af94
Gabe Fierro <gtfierro@mines.edu>
2026-01-26 09:09:54 -0700

    update build

--- build_examples.sh
+++ build_examples.sh
@@ -4,11 +4,11 @@ set -ex
 # for debugging ontoenv
 export RUST_BACKTRACE=1
 
-ONTOENV_LOG=info ontoenv init models ontologies -i '*.ttl' -e 'models/withimports/*.ttl' -e 'models/compiled/*.ttl' --overwrite
+ONTOENV_LOG=info uv run ontoenv init -i '*.ttl' -e 'models/withimports/*.ttl' -e 'models/compiled/*.ttl' --overwrite -- models ontologies
 
 
 # for each filename in the models/ directory compile models and pull imports
-ontoenv update
+uv run ontoenv update
 mkdir -p models/compiled
 mkdir -p models/withimports
 make -j 2
